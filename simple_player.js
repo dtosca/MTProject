30ba6f39ff0a40218e5bf3e896c4c8967c859570
Diana Tosca <ditov@LAPTOP-VA3AQSQ5.mit.edu>
2017-12-03 19:35:00 -0500

    Updating files

--- simple_player.js
+++ simple_player.js
@@ -2,13 +2,46 @@
 // m_dspNetwork.get returns raw pointer to 
 
 var dsp = $.app.dspNetwork();
+var root = $.app.mainLayer();
 
-if (dsp.isRunning()) {
-	console.log("I will playback the track now. You should hear sound from left speaker");
-	dsp.samplePlayer().playSample("ElectricSweep.wav", 1.0, 1.0, 0, 0);
+//slider not working, ask Lauren later
+var slider = new MultiWidgets.SliderWidget();
+slider.setLocation(0,0);
+slider.setMinVal(0);
+slider.setMaxVal(10);
+slider.setCurrentValue(5);
+slider.setStep(1.0);
+slider.setWidth(100);
+slider.raiseToTop();
+root.addChild(slider);
 
-	console.log("playing second sample too...");
-	dsp.samplePlayer().playSample("PinkPanther30.wav", 1.0, 1.0, 0, 0);
-} else {
-	console.log("Error");
+//plays one sound
+var playElectricSweep = addBttn(100,100,100,100,"play.png","ElectricSweep.wav");
+var playPinkPanther = addBttn(300,100,100,100,"play.png","PinkPanther30.wav");
+
+//plays another sound
+function addBttn(x, y, sizeX, sizeY, image,soundFile) {
+	var w = new MultiWidgets.JavaScriptWidget();
+
+	w.setLocation(x, y);
+	w.setWidth(sizeX);
+	w.setHeight(sizeY);
+	w.img = new MultiWidgets.ImageWidget();
+
+	if (w.img.load(image)) {
+	    w.img.addOperator(new MultiWidgets.StayInsideParentOperator());
+    	w.img.setLocation(x,y);
+    	w.img.setWidth(sizeX);
+	    w.img.setHeight(sizeY);
+	    w.addChild(w.img);
+    	w.img.raiseToTop();
+    	w.setFixed();
+	}
+
+	w.onSingleTap(function(){
+		dsp.samplePlayer().playSample(soundFile, 1.0, 1.0, 0, 0);
+	});
+
+	root.addChild(w);
+	w.raiseToTop();
 }
